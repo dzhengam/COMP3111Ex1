113012973533b8fe245c2431b208ee3b7eb870d5
dzhengam <dzhengam@connect.ust.hk>
2026-02-12 22:01:21 +0800

    This is an initial commit for COMP3111 project

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,7 +7,6 @@ public class mainApp1 {
         int b = 2;
         int n = 11;
         System.out.println("Program starting .....");
-        System.out.println("done");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
